784ee8a6adc14a0cb7f2aa21a971a4b0b6f7c41f
Complexia <complexia701@gmail.com>
2024-04-16 10:43:31 +1000

    updated openzeppeling contracts

--- contracts/src/JWTContract.sol
+++ contracts/src/JWTContract.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.23;
 
 import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
-contract NFT is ERC721 {
+contract JWTContract is ERC721 {
     uint256 public currentTokenId;
 
-    constructor() ERC721("NFT Name", "NFT") {}
+    constructor() ERC721("JWT Contract", "JWT") {}
 
     function mint(address recipient) public payable returns (uint256) {
         uint256 newItemId = ++currentTokenId;
