f01ac96c6714a363314e1beb3532887da3edd044
Complexia <complexia701@gmail.com>
2024-04-16 12:06:42 +1000

    updated JWT token/removed Counter.sol dependency

--- contracts/src/JWTContract.sol
+++ contracts/src/JWTContract.sol
@@ -12,14 +12,28 @@ import "./Counter.sol";
 contract JWTContract is ERC721 {
     uint256 public currentTokenId;
 
-    Counter private _tokenIdCounter;
-
     string private _tokenURI;
     uint256 private mintPrice;
     uint256 private mintCount;
     bool private allowMint;
     address private owner;
-    mapping(address => bool) private minters;
+    address private factoryContract;
+
+    constructor(
+        string memory _uri,
+        uint256 _mintCount,
+        uint256 _tokenPrice,
+        bool _allowMint,
+        address creator,
+        address _factoryContract
+    ) ERC721("JWTToken", "JWT") {
+        mintCount = _mintCount;
+        _tokenURI = _uri;
+        mintPrice = _tokenPrice;
+        owner = creator;
+        allowMint = _allowMint;
+        factoryContract = _factoryContract;
+    }
 
     // Event triggered when deploying the contract
     event Initialized(
@@ -30,44 +44,8 @@ contract JWTContract is ERC721 {
         bool _allowMints
     );
 
-    // Event triggered when minting starts
-    event MintStarted(uint256 timestamp);
-
-    // Event triggered when minting is paused
-    event MintPaused(uint256 timestamp);
-
-    // Event triggered when a token URI is updated
-    event TokenURIUpdated(string newUri);
-
-    // Event triggered when the price is updated
-    event TokenPriceUpdated(uint256 newPrice);
-
-    // Event triggered when the mint count is updated
-    event MintCountUpdated(uint256 newMintCount);
-
-    // Event triggered when a token is minted
-    event TokenMinted(address indexed account, uint256 indexed tokenId, uint256 timestamp);
-
-    
-
-    
-
-    constructor(
-        string memory _uri,
-        uint256 _mintCount,
-        uint256 _tokenPrice,
-        bool _allowMint,
-        address creator
-    ) ERC721("JWTToken", "JWT") {
-        mintCount = _mintCount;
-        _tokenURI = _uri;
-        mintPrice = _tokenPrice;
-        owner = creator;
-        emit Initialized(_uri, _mintCount, _tokenPrice, creator, _allowMint);
-    }
-
     modifier onlyOwner() {
-        require(msg.sender == owner, "Unauthorized");
+        require(tx.origin == owner && msg.sender == factoryContract, "Unauthorized");
         _;
     }
 
@@ -75,19 +53,59 @@ contract JWTContract is ERC721 {
      * @dev Start the minting process.
      */
     function startMint() public onlyOwner {
-        require(!allowMint, "Mint started");
+        require(!allowMint, "Mint already started");
         allowMint = true;
-        emit MintStarted(block.timestamp);
     }
 
     /**
      * @dev Pause the minting process.
      */
     function pauseMint() public onlyOwner {
-        require(allowMint, "Mint paused");
+        require(allowMint, "Mint already paused");
         allowMint = false;
-        emit MintPaused(block.timestamp);
     }
+
+    /**
+     * @dev Set a new URI for a token.
+     * @param _newUri The new URI.
+     */
+    function setURI(string memory _newUri) public onlyOwner {
+        _tokenURI = _newUri;
+    }
+
+    /**
+     * @dev Set a custom price for a token.
+     * @param _price The new price for the token.
+     */
+    function setMintPrice(uint256 _price) public onlyOwner {
+        // can be free/should be free
+        mintPrice = _price;
+    }
+
+    /**
+     * @dev Set the mint count for a token.
+     * @param _mintCount The new mint count for the token.
+     */
+    function setMintCount(uint256 _mintCount) public onlyOwner {
+        require(_mintCount >= currentTokenId, "Invalid count");
+        mintCount = _mintCount;
+    }
+
+    /**
+     * @dev Mint a token to the specified account.
+     * @param to The recipient of the minted token.
+     * @return tokenId The ID of the minted token.
+     */
+    function mint(address to) public payable returns (uint256 tokenId) {
+        require(allowMint, "Minting is currently paused");
+        require(msg.value >= mintPrice, "Insufficient funds to mint");
+        require(balanceOf(to) == 0, "Only one token per account is allowed");
+        tokenId = currentTokenId;
+        currentTokenId++;
+        _safeMint(to, tokenId);
+    }
+
+    // Getter Functions
 
     /**
      * @dev Returns the URI for a given token.
@@ -98,46 +116,28 @@ contract JWTContract is ERC721 {
     }
 
     /**
-     * @dev Set a new URI for a token.
-     * @param _newUri The new URI.
+     * @dev Returns the mint price for a token.
+     * @return The mint price.
      */
-    function setURI(string memory _newUri) public onlyOwner {
-        _tokenURI = _newUri;
-        emit TokenURIUpdated(_newUri);
+    function getTokenMintPrice() public view returns (uint256) {
+        return mintPrice;
     }
 
     /**
-     * @dev Set a custom price for a token.
-     * @param _price The new price for the token.
+     * @dev Returns the mint count for a token.
+     * @return The mint count.
      */
-    function setMintPrice(uint256 _price) public onlyOwner {
-        // can be 0, should be zero
-        mintPrice = _price;
-        emit TokenPriceUpdated(_price);
+    function getTokenMintCount() public view returns (uint256) {
+        return mintCount;
     }
 
     /**
-     * @dev Set the mint count for a token.
-     * @param _mintCount The new mint count for the token.
+     * @dev Returns the current minting availability status.
+     * @return The minting status.
      */
-    function setMintCount(uint256 _mintCount) public onlyOwner {
-        require(_mintCount > 0, "Invalid count");
-        mintCount = _mintCount;
-        emit MintCountUpdated(_mintCount);
+    function getAllowMint() public view returns (bool) {
+        return allowMint;
     }
 
-    /**
-     * @dev Mint a token to the specified account.
-     * @param to The recipient of the minted token.
-     */
-    function mint(address to) public payable /*onlyMinter*/
-    {
-        require(allowMint, "Mint Paused");
-        require(msg.value >= mintPrice, "Insufficient Funds");
-        require(balanceOf(to) == 0, "One token per account");
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        emit TokenMinted(to, tokenId, block.timestamp);
-    }
+    
 }
