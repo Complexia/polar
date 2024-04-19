83ac91ea840059507ffe801a3dc7636eeb1cafff
Complexia <complexia701@gmail.com>
2024-04-19 21:14:52 +1000

    another psh

---
+++ polar/app/auth/magic/auth-txn/page.tsx
@@ -0,0 +1,28 @@
+// oauth redirects here
+
+import AuthMagicComponent from "@/components/auth-component"
+
+
+const AuthTxn = (context) => {
+    console.log("params", context.searchParams)
+    // params need to be message to sign and nonce
+    // get message and nonce from params
+    
+    let redirect_url = context.searchParams["redirect_url"]
+    // contact address
+    // contract abi
+    // contract method
+    
+    console.log("redirect_url", redirect_url)
+
+    
+    // sign message and nonce
+    
+    return (
+       <div>
+        
+       </div>
+    )
+}
+
+export default AuthTxn;
